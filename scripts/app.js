39f20bfbabc48d6e52d6ed79440e896886e94911
quaynebrown <quayne.brown@gmail.com>
2020-11-10 17:44:44 -0500

    added scramble characters feature

--- scripts/app.js
+++ scripts/app.js
@@ -3,15 +3,15 @@
 // Let the user enter their own words then unscramble the words for the user to spell.
 
 // MVP
-// 1. the user input an infinte number of words
+// 1. the user input an infinite number of words
 // 2. the user to submit the words
-// 3. make a copy of the original array (map)
-// 3a. randomize each character in each word
+// 3. randomize each character in each word and store the word in a new array
 // 4. generate html that shows the user the randomized words and allow the user to input the correct spelling
-// 5. calculate the result
+// 5. The user submit their answers
+// 6. calculate the result
 
 // Stretch goals
-// 1. Allow the user to select if they want to unscrable the words or if they want to fill in the blanks instead.
+// 1. Allow the user to select if they want to unscramble the words or if they want to fill in the blanks instead.
 // 2. Show a list of words that the user has entered after they add a new word
 // 3. Add play again feature
 
@@ -22,60 +22,73 @@
     // display the total number of words entered
     // clear the input field and add focus
 
+// 2. Add event listener for when the user click 'Submit & Play!'
+    // scramble the words and store in a new array
+    // Take the user to a new page to unscramble the words
+    // generate a list of inputs for the user to unscramble their words
     
-    // 3. Add event listener for when the user click 'Submit your answers'
+// 3. Add event listener for when the user click 'Submit your answers'
     // compare the user's input with the words that are in the original array
     // Calculate how much they got right
     // calculate the percentage
     // display result
-    
-    // 4. 
-    
-    
-    
-    const spellingApp = {};
-    spellingApp.words = [];
-    spellingApp.listCount = 0;
-
-    spellingApp.scrambleletters = (toArray, callback) =>{
-
-        if(Array.isArray(toArray)){
-
-        }else{
-            let result = '';
-
-            for(let i = 0; i < toArray.length; i++){
-                //TODO: NEED TO FIGURE OUT HOW TO SCRAMBLE STRING
-            }
-        }
-    }
-    
-    
-    //get the amount of <li>'s currently on the page
-    spellingApp.getAmountOfLi = function() {
-        spellingApp.listCount = spellingApp.listCount ? $('li').length + 1 : 0;
-        console.log(spellingApp.listCount)
-    }
-    
-    
-    // 2. Add event listener for when the user click 'Submit & Play!'
-    $('.letsPlay').on('click', function(e){
-        e.preventDefault();
-        // scramble the words and store in a new array
         
-        // Take the user to a new page to unscramble the words
+    
+    
+    
+const spellingApp = {};
+spellingApp.words = [];
 
-        // generate a list of inputs for the user to unscramble their words
-    });
+
+// take a word and shuffles the characters
+spellingApp.shuffleLetters =  (word) => {
+    let wordLength = word.length;
+    let scrambledWord = '';
+
+    //character array
+    const charArray = word.split('');
+
+    while(wordLength !== 0){
+        const randomNumber = Math.floor(Math.random() * wordLength);
+        scrambledWord += charArray[randomNumber];
+        charArray.splice(randomNumber, 1); // remove 1 character from random index
+        wordLength--;
+    }
+
+    // call back the function if the word did not scramble
+    if(spellingApp.words.includes(scrambledWord)){
+        spellingApp.shuffleLetters(scrambledWord);
+    }
+    console.log(scrambledWord)
+    return scrambledWord;
+}
+
+
+//get the amount of <li>'s currently on the page
+spellingApp.getAmountOfLi = function() {
+    spellingApp.listCount = spellingApp.listCount ? $('li').length + 1 : 0;
+    console.log(spellingApp.listCount)
+}
+
+
+// 2. Add event listener for when the user click 'Submit & Play!'
+$('.letsPlay').on('click', function(e){
+    e.preventDefault();
+    // scramble the words and store in a new array
+    
+    // Take the user to a new page to unscramble the words
+
+    // generate a list of inputs for the user to unscramble their words
+});
     
     
-    const $userInput = $('#user-word');
-    const $wordCount = $('.word-count');
-    // 1. Add event listener for when the user click 'add word'
-    $('.add-word').on('click', function(e){
-        e.preventDefault();
-        const word = $userInput.val();
-        // push each word to a words array
+const $userInput = $('#user-word');
+const $wordCount = $('.word-count');
+// 1. Add event listener for when the user click 'add word'
+$('.add-word').on('click', function(e){
+    e.preventDefault();
+    const word = $userInput.val();
+    // push each word to a words array
     spellingApp.words.push(word);
 
     // clear the input field and add focus
@@ -91,8 +104,11 @@ spellingApp.getWordCount = () => {
     $wordCount.text(spellingApp.words.length + 1);
 }
 
+
+
 spellingApp.init = function (){
     spellingApp.getWordCount();
+    $('.letsPlay').attr('disabled', true);
 }
 
 // wait for DOM to be ready
