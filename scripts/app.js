7495e8dd17835d534650ab70413360ee256a6878
quaynebrown <quayne.brown@gmail.com>
2020-11-15 18:29:59 -0500

    removed Extraneous

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,3 @@
-// make an app that:
-// Description of project
-// Let the user enter their own words then unscramble the words for the user to spell.
-
-// MVP
-// 1. the user input an infinite number of words
-// 2. the user to submit the words
-// 3. randomize each character in each word and store the word in a new array
-// 4. generate html that shows the user the randomized words and allow the user to input the correct spelling
-// 5. The user submit their answers
-// 6. calculate the result
-
-// Stretch goals
-// 1. Allow the user to select if they want to unscramble the words or if they want to fill in the blanks instead.
-// 2. Show a list of words that the user has entered after they add a new word
-// 3. Add play again feature
-
 // pseudoCode
 // 1. Add event listener for when the user click 'add word'
     // push each word to a words array
@@ -37,9 +20,11 @@
 const spellingApp = {};
 spellingApp.words = [];
 spellingApp.scrambledWords = [];
+
+//font awesome ID counter
 spellingApp.faIdCount = 0;
 
-// take a word and shuffles the characters
+// take a word and shuffle the characters
 spellingApp.shuffleLetters =  (word) => {
     let wordLength = word.length;
     let scrambledWord = '';
@@ -47,6 +32,8 @@ spellingApp.shuffleLetters =  (word) => {
     //character array
     const charArray = word.split('');
 
+    // take a random character from the given word and concat to a new string then
+    // delete that character from the array
     while(wordLength !== 0){
         const randomNumber = Math.floor(Math.random() * wordLength);
         scrambledWord += charArray[randomNumber];
@@ -55,7 +42,7 @@ spellingApp.shuffleLetters =  (word) => {
     }
 
     // call back the function if the word did not scramble
-    if(word === scrambledWord){
+    if(word == scrambledWord){
         spellingApp.shuffleLetters(scrambledWord);
     }
 
@@ -80,6 +67,7 @@ spellingApp.calculatePercentage = (correctAnswers = 0) => {
     return result.toFixed(2);
 }
 
+// reset the game when the user press 'new game'
 spellingApp.resetGame = () => {
     spellingApp.words = [];
     spellingApp.scrambledWords = [];
@@ -90,13 +78,21 @@ spellingApp.resetGame = () => {
     $('#scrambledWords').empty();
 }
 
+// display the result after the user submit their answers
 spellingApp.displayResult = (correctAnswersCount) => {
     // display result
     $('.result span').text(`${correctAnswersCount}/${spellingApp.words.length}`);
     $('.result-percentage span').text(spellingApp.calculatePercentage(correctAnswersCount) +'%');
 }
 
-// Display forms to display the initial form for the user to enter new words
+// set word count and display them on the page
+spellingApp.setWordCount = () => {
+    $wordCount.text(spellingApp.words.length + 1);
+    $wordIndex.text(spellingApp.words.length);
+}
+
+// Swap between the two forms
+// show form 1 and hide form 2
 spellingApp.swapForms1st = () => {
     $('#form-1-container').removeClass('hide-section')
     .attr('aria-hidden', true);
@@ -104,7 +100,8 @@ spellingApp.swapForms1st = () => {
     .attr('aria-hidden', false);
 }
 
-// Display forms to display the the second form for the user to spell their words to be compared
+// swap between the two forms
+// show form 2 and hide form 1
 spellingApp.swapForms2nd = () => {
     $('#form-1-container').addClass('hide-section')
     .attr('aria-hidden', true);
@@ -112,24 +109,35 @@ spellingApp.swapForms2nd = () => {
     .attr('aria-hidden', false);
 }
 
+// Allow the user to delete a word after they click on the 'fa-minus-circle'
 $('.words').on('click', '.fa-minus-circle', function() {
+    // store the text that is in the <p> tag next to 'this' fa-minus-circle
     const wordToDelete = $(this).siblings('p').text();
+
+    // find the index in the array that contains this word
     const indexToDelete = spellingApp.words.findIndex((element) => {
         return element.toLowerCase() == wordToDelete.toLowerCase();
     });
+
+    // delete 1 word from the given index
     spellingApp.words.splice(indexToDelete, 1);
+
+    // update the new word count
     spellingApp.setWordCount();
+
+    // remove the container of the deleted word
     $(this).parent().removeClass('list-item').empty();
 })
 
-// new game
+// event listener for when the user click 'new game'
 $('#new-game').on('click', function() {
     spellingApp.resetGame();
 });
 
-// play again
+// event listener for when the user click 'play again'
 $('#play-again').on('click', function() {
     $('#answer-submit').attr('disabled', false).removeClass('disable');
+    
     // clear all input field
     $('.unscrambled-word').each(function (){
         const unscrambledWord = $(this).val('');
@@ -145,7 +153,7 @@ $('#play-again').on('click', function() {
     $('.result-percentage span').text('0.00%')
 });
 
-// Add event listener for when the user click 'Submit your answers'
+// event listener for when the user click 'Submit your answers'
 $('#form-2').on('submit', function(e){
     e.preventDefault();
     let correctAnswersCount = 0;
@@ -155,6 +163,7 @@ $('#form-2').on('submit', function(e){
         const unscrambledWord = $(this).val().toLowerCase().trim();
 
         $(this).attr('disabled', true);
+
         // compare the user's input with the words that are in the original array
         if (spellingApp.scrambledWords[index].userWord == unscrambledWord){
             // Calculate how much they got right
@@ -170,14 +179,14 @@ $('#form-2').on('submit', function(e){
         index++;
     });
 
-    $('#answer-submit').attr('disabled', true).addClass('disable');
+    // $('#answer-submit').attr('disabled', true).addClass('disable');
     $('#play-again').attr('disabled', false).removeClass('disable');
 
     // display result
     spellingApp.displayResult(correctAnswersCount);
 })
 
-// 2. Add event listener for when the user click 'Submit & Play!'
+// 2. event listener for when the user click 'Let's Play!'
 $('.letsPlay').on('click', function(e){
     e.preventDefault();
     // scramble the words and store in a new array
@@ -233,23 +242,15 @@ $('#form-1').on('submit', function(e){
         $('.err-container').html('<p>You did not enter a word!</p>')
     }
 });
-
-// set word count on the page
-spellingApp.setWordCount = () => {
-    $wordCount.text(spellingApp.words.length + 1);
-    $wordIndex.text(spellingApp.words.length);
-}
+0
 
 // initializing app
 spellingApp.init = function (){
     spellingApp.setWordCount() ;
     $('.letsPlay, #play-again').attr('disabled', true).addClass('disable');
-    // $('#form-2-container').addClass('show-section');
 }
 
 // wait for DOM to be ready
 $(() => {
     spellingApp.init();
 })
-
-//TODO: create a json file that stores the predefined words and have the ability to add new words from the user input
